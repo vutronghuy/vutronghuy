4d8f89c3df810f8bf7be28d85bb66b5c01b63d0f
vutronghuy <vutronghuygrw24092003@gmail.com>
2023-04-19 01:43:49 +0700

    modified:   resources/views/about.blade.php

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -37,5 +37,9 @@
 </div>
 <div>Google map
 </div>
+<div class="map">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.743415826598!2d105.76362570954073!3d21.042950180529033!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313454b6071d2787%3A0xb99aa77c8ddca157!2zMTM2IMSQLiBI4buTIFTDuW5nIE3huq11LCBNYWkgROG7i2NoLCBC4bqvYyBU4burIExpw6ptLCBIw6AgTuG7mWksIFZpZXRuYW0!5e0!3m2!1sen!2s!4v1681842534829!5m2!1sen!2s" 
+    width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+</div>
 </body>
 </html>
