0307c1e747f8215ea2a92b36fa3f59a440a06b82
vutronghuy <vutronghuygrw24092003@gmail.com>
2023-04-19 14:14:30 +0700

    new file:   public/hot 	modified:   resources/js/app.js 	modified:   resources/js/bootstrap.js 	deleted:    resources/sass/_variables.scss 	deleted:    resources/sass/app.scss 	deleted:    resources/views/auth/login.blade.php 	deleted:    resources/views/auth/passwords/confirm.blade.php 	deleted:    resources/views/auth/passwords/email.blade.php 	deleted:    resources/views/auth/passwords/reset.blade.php 	deleted:    resources/views/auth/register.blade.php 	deleted:    resources/views/auth/verify.blade.php 	deleted:    resources/views/carts/add.blade.php 	deleted:    resources/views/carts/index.blade.php 	deleted:    resources/views/carts/remove.blade.php 	deleted:    resources/views/carts/update.blade.php 	deleted:    resources/views/layouts/app.blade.php 	deleted:    resources/views/products/app.blade.php 	deleted:    resources/views/products/create.blade.php 	deleted:    resources/views/products/edit.blade.php 	deleted:    resources/views/products/index.blade.php 	deleted:    resources/views/products/show.blade.php 	deleted:    resources/views/search/index.blade.php 	modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -44,6 +44,9 @@ Route::get('/contact', function () {
 Route::get('login', function () { 
     return view('auth.login');
 })->name('login');
+Route::get('register', function () { 
+    return view('auth.register');
+})->name('register');
 
 // Auth::routes();
 
