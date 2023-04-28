f2d76a037f064a54ac174f218d401c296521114b
N0name1011 <namhungvn2003@gmail.com>
2023-04-29 02:10:08 +0700

    modified:   resources/views/products/show.blade.php

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -14,7 +14,7 @@
                 <li><a href="/home">Home</a></li>
                 <li><a href="/menu">Menu</a></li>
                 <li><a href="/about">About</a></li>
-                <li><a href="/cart">Cart</a></li>
+                <li><a href="/shopping_cart">Cart</a></li>
                 <li><a href="/contact">Contact</a></li>
             </ul>
         </nav>
